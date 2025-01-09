709deac31737f386915fb900335ae3448a6c77bc
diachkinainna <diachkinainna@gmail.com>
2025-01-09 21:59:14 +0200

    add Air landing

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+# Air landing
+
+## Short description of the project
+
+The page is designed with a responsive layout that adapts to the screens of both desktop and mobile devices.
+Hover effects add dynamism to the user experience.
+
+### Screen Adaptations:
+
++ Large screen >2560px
++ Desktop >1600px
++ Small desktop >1024px
++ Tablet >640px
++ Mobile >320px
+
+## Technologies used
+
+[![My Skills](https://skillicons.dev/icons?i=html,css,sass,js,github,vscode,figma)](https://skillicons.dev)
+
+## Link to the preview
+
+[DEMO LINK](https://diachkinainna.github.io/air-landing/)
+
+## Link to the design
+
+[NOTHING](https://www.figma.com/design/7qwsWggv9BAxMi2VPhBuPr/Air-(formerly-Dia)?node-id=0-1&p=f&t=zaU3D095DnFXGcYL-0)
+
+## Instructions for running the project
+
++ Clone the repository in your PC.
++ Run `npm install` to install the dependencies.
++ Run `npm start` to view the landing page.
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,65 @@
 'use strict';
+
+const dropdown = document.querySelector('.top-bar__dropdown');
+const trigger = document.querySelector('.top-bar__menu');
+const navList = document.querySelector('.nav--dropdown');
+
+let timeoutId;
+
+trigger.addEventListener('mouseenter', () => {
+  dropdown.classList.add('dropdown--active');
+  clearTimeout(timeoutId);
+});
+
+dropdown.addEventListener('mouseleave', () => {
+  timeoutId = setTimeout(() => {
+    dropdown.classList.remove('dropdown--active');
+  }, 500);
+});
+
+navList.addEventListener('mouseenter', () => {
+  clearTimeout(timeoutId);
+});
+
+navList.addEventListener('mouseleave', () => {
+  timeoutId = setTimeout(() => {
+    dropdown.classList.remove('dropdown--active');
+  }, 500);
+});
+
+const slider = document.querySelector('.slider');
+const right = document.querySelector('.arrow--right');
+const left = document.querySelector('.arrow--left');
+let count = 0;
+
+right.addEventListener('click', () => {
+  if (count === 0) {
+    slider.classList.add('slider--right');
+    right.style.cursor = 'initial';
+    right.style.opacity = 0.3;
+    count++;
+  }
+
+  if (count === -1) {
+    slider.classList.remove('slider--left');
+    left.style.opacity = 1;
+    left.style.cursor = 'pointer';
+    count++;
+  }
+});
+
+left.addEventListener('click', () => {
+  if (count === 0) {
+    slider.classList.add('slider--left');
+    left.style.opacity = 0.3;
+    left.style.cursor = 'initial';
+    count--;
+  }
+
+  if (count === 1) {
+    slider.classList.remove('slider--right');
+    right.style.cursor = 'pointer';
+    right.style.opacity = 1;
+    count--;
+  }
+});
